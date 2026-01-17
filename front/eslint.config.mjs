a6a85fa6d196a6f9970147b00dbeabc0c70dd6be
Daniel Sanabria <danielsanabriab255@gmail.com>
2026-01-17 17:29:31 -0500

    feat: actualizadas las reglas de eslint para mejorar la calidad del codigo

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import stylistic from '@stylistic/eslint-plugin';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      "no-unused-expressions": "error",
+      "no-unused-vars": "error",
+      "no-console": "error",
+      "@stylistic/semi": "error",
+      "@stylistic/indent": ["error",2]
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
